255c52ff06c7e2f8add1b964a6b886faa4e98dff
Torrel <taurel107@gmail.com>
2021-08-11 08:13:16 -0400

    stored data in an object

--- inspiration_quotes.js
+++ inspiration_quotes.js
@@ -1,7 +1,31 @@
+
 function inspiration() {
 
-     const quotes = ["Life Loves Me!", "As I say Yes to Life, life says yes to me.", 
-                     "I prosper whereever I turn","I prosper wherever I turn." ]
-     return quotes[Math.floor(Math.random() * 4)]                
+     let randomQuotes = {
+       dailyQuotes : ["Life Loves Me!", "As I say Yes to Life, life says yes to me.", 
+           "I prosper whereever I turn", "I prosper wherever I turn." ],
+       affirmations : ["I trust the process of life.", "Today is a scared gift from life.", "Only good can come to me."],
+
+       get_day() {
+        const d = new Date();
+        const weekday = new Array(7)
+    
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday"
+    
+        const day = weekday[d.getDay()]
+        return day;
+
+       },
+       
+    };
+     
+   return `Today is ${randomQuotes.get_day()}, ${randomQuotes.dailyQuotes[Math.floor(Math.random() * randomQuotes.dailyQuotes.length)]}, 
+           ${randomQuotes.affirmations[Math.floor(Math.random() * randomQuotes.affirmations.length)]}`                
 }
 console.log(inspiration())
